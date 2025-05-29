0c20fe5e71e403c5226b7d8685b6263a89a6b8eb
Sameen Karim <sameenkarim@gmail.com>
2025-05-28 21:28:30 -0400

    simplified test runner

---
+++ tests/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const { spawn } = require('child_process');
+
+// Dynamically find all test files
+const testFiles = fs.readdirSync(__dirname)
+  .filter(file => file.endsWith('.test.js'))
+  .map(file => path.join(__dirname, file));
+
+console.log('Running tests...\n');
+console.log('Found test files:', testFiles.map(f => path.basename(f)).join(', '), '\n');
+
+// Run all tests with flag for WASM support
+const tests = spawn('node', [
+  '--experimental-vm-modules',
+  '--test',
+  ...testFiles
+], {
+  stdio: 'inherit'
+});
+
+tests.on('close', (code) => {
+  process.exit(code);
+});
